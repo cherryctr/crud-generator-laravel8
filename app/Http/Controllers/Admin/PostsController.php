a4231d365ab613864c11d63089ee4c1275efb4bb
chrryctr <chrryctr1509@gmail.com>
2022-09-20 15:07:53 +0700

    first commit

---
+++ app/Http/Controllers/Admin/PostsController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $posts = Post::where('title', 'LIKE', "%$keyword%")
+                ->orWhere('content', 'LIKE', "%$keyword%")
+                ->orWhere('category', 'LIKE', "%$keyword%")
+                ->latest()->paginate($perPage);
+        } else {
+            $posts = Post::latest()->paginate($perPage);
+        }
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+
+        $requestData = $request->all();
+        $validated = $this->validate($request, [
+            "title" => "required",
+            "content" => "required|min:10",
+            "category" => "required",
+            // "email" => "required|email|unique:users,email",
+            // "password" => "required|min:8|max:100"
+        ]);
+
+        if($requestData == true) {
+            Post::create($requestData);
+        }else{
+            Post::create($validated);
+        }
+
+
+        return redirect('admin/posts')->with('flash_message', 'Post added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, $id)
+    {
+
+        $validated = $this->validate($request, [
+            "title" => "required",
+            "content" => "required|min:10",
+            "category" => "required",
+            // "email" => "required|email|unique:users,email",
+            // "password" => "required|min:8|max:100"
+        ]);
+
+        $requestData = $request->all();
+
+        $post = Post::findOrFail($id);
+
+        if($requestData == true) {
+            $post->update($requestData);
+        }else{
+            $post->update($requestData);
+        }
+
+
+        return redirect('admin/posts')->with('flash_message', 'Post updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        Post::destroy($id);
+
+        return redirect('admin/posts')->with('flash_message', 'Post deleted!');
+    }
+}
